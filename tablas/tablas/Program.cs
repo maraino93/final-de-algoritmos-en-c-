ef3042e69faa7f2a86cc02b2ca28d1be6b91e147
mariano sanchez <114032766+maraino93@users.noreply.github.com>
2023-01-18 16:11:04 -0300

    Add files via upload

---
+++ tablas/tablas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablas
+{
+    internal class Program
+    {
+        /*1.11.2: Pedir al usuario un número y mostrar su tabla de multiplicar,
+         * usando {0},{1} y {2}. Por ejemplo, si el número es el 3,
+         * debería escribirse algo como
+         3 x 0 = 0
+         3 x 1 = 3
+         3 x 2 = 6*/
+        static void Main(string[] args)
+        {
+            int num1;
+            num1 = int.Parse(Console.ReadLine());
+            for (int i= 1; i <= 10; i++)
+            {
+               Console.WriteLine(num1+"x"+i+"="+num1*i);
+            }
+           Console.ReadKey();
+           
+           
+
+
+        }
+    }
+}
